cf78f51a0ac0a3e4624fa90ffcf0fa91fd4db55e
m1070441 <110035050+m1070441@users.noreply.github.com>
2022-08-01 17:09:28 +0530

    davis99 device added

--- Models/DeviceProperties.cs
+++ Models/DeviceProperties.cs
@@ -14,7 +14,7 @@ namespace IoTDevices.Models
         static string connectionString = "HostName=NxTIoTTraining.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=RlHaCMxWGXDfNA+0BPe7b8WPjOnS+7B7EUAfBQSMOzg=";
         static RegistryManager registryManager= RegistryManager.CreateFromConnectionString(connectionString);
         static DeviceClient Client = null;
-        static string DeviceConnectionString= "HostName=NxTIoTTraining.azure-devices.net;DeviceId=davisdt;SharedAccessKey=7rN3Zev33PXuHSBUvpir6e6uRL2oPbtH75Q2MPInzUM=";
+        static string DeviceConnectionString= "HostName=NxTIoTTraining.azure-devices.net;DeviceId=davis77;SharedAccessKey=sDToLiP88HRaJqROl4YzYn03wz9UXJJMNc2NsQPvDUY=";
 
         public static async Task UpdateReportedProperties(string deviceName,ReportedProperties properties)
         {
